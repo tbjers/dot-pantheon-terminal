3c4ac2a2846d7536c0d61f267541542facff28cb
Torgny Bjers <torgny.bjers@gmail.com>
2017-09-02 12:22:02 -0400

    Removed bold-color, key doesn't exist

--- ellipsis.sh
+++ ellipsis.sh
@@ -9,7 +9,6 @@ pkg.install() {
         gsettings set org.pantheon.terminal.settings palette "#000000:#E06C75:#98c379:#d19a66:#61aeee:#c678dd:#56b6c2:#abb2bf:#5c6370:#e06c75:#98c379:#d19a66:#62afee:#c678dd:#56b6c2:#ffffff"
         gsettings set org.pantheon.terminal.settings foreground "#abb2bf"
         gsettings set org.pantheon.terminal.settings background "#282c34"
-        gsettings set org.pantheon.terminal.settings bold-color "#abb2bf"
         gsettings set org.pantheon.terminal.settings follow-last-tab "true"
       fi
       ;;
