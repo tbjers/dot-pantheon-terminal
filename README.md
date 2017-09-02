af15f45b2b567f0e02df1d8c42c260ba5eae9b8e
Torgny Bjers <torgny.bjers@gmail.com>
2017-09-02 12:19:25 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,12 @@
+# tbjers/dot-pantheon-terminal
+My [Pantheon Terminal][pantheon-terminal] dotfiles, [ellipsis][ellipsis] compatible.
+
+## Install
+Clone and symlink or install with [ellipsis][ellipsis]:
+
+```
+$ ellipsis install tbjers/dot-pantheon-terminal
+```
+
+[ellipsis]: http://ellipsis.sh
+[pantheon-terminal]: https://github.com/elementary/terminal
---
+++ ellipsis.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# tbjers/dot-pantheon-terminal ellipsis package
+
+pkg.install() {
+  case $(os.platform) in
+    linux)
+      if utils.cmd_exists gsettings; then
+        gsettings set org.pantheon.terminal.settings palette "#000000:#E06C75:#98c379:#d19a66:#61aeee:#c678dd:#56b6c2:#abb2bf:#5c6370:#e06c75:#98c379:#d19a66:#62afee:#c678dd:#56b6c2:#ffffff"
+        gsettings set org.pantheon.terminal.settings foreground "#abb2bf"
+        gsettings set org.pantheon.terminal.settings background "#282c34"
+        gsettings set org.pantheon.terminal.settings bold-color "#abb2bf"
+        gsettings set org.pantheon.terminal.settings follow-last-tab "true"
+      fi
+      ;;
+  esac
+}
